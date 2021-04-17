ca1513354ab41ff1bcd2f7914605c0e7e23694d8
kisnan <58051473+kisnan@users.noreply.github.com>
2021-04-16 14:44:32 -1000

    upload
    Assignmant1

--- lab11/Ex6.js
+++ lab11/Ex6.js
@@ -8,7 +8,7 @@ function isNonNegInt(q, returnErrors=false) {
     if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
     if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
 
-    return returnErrors ? errors : (errors.length == 0);
+    return returnErrors ? errors : ((errors.length > 0)? false:true);
 }
 
 pieces = 'Qiyan;23;-23;23.5'.split(";");
