b90a82b524b1aba35027d8a5074e454c9d8b1814
kisnan <58051473+kisnan@users.noreply.github.com>
2021-05-14 23:43:22 -1000

    backup
    backup

---
+++ Assignment3_2/public/camera_product.js
@@ -0,0 +1,86 @@
+var products = [
+    {
+        "camera1" : "FX3 Cinema Line camera",
+    },
+    {
+        "camera2" : "a5100 Beginner E-mount camera ",
+    },
+    {
+        "camera3" : "a9 Full-frame camera",
+    },
+    {
+        "camera4" : "a1 Flagship camera",
+    },
+    {
+        "camera5" : "A7c Full-frame camera",
+    }
+]
+
+var FX3CinemaLinecamera = [
+    {
+        "name": "FX3 Cinema Line camera Camera Only",
+        "price": 100,
+        "image": "Fx3.jpg"
+    },
+    {
+        "name": "FX3 Cinema Line camera w/SD card",
+        "price": 110,
+        "image": "Fx3.jpg"
+    },
+    {
+        "name": "FX3 Cinema Line camera w/Tripod",
+        "price": 120,
+        "image": "Fx3.jpg"
+    }
+]
+var BeginnerCameraa5100 = [
+    {
+        "name": "a5100 Beginner E-mount camera",
+        "price": 10,
+        "image": "a5100.jpg"
+    },
+    {
+        "name": "a5100 Beginner E-mount camera w/SD card",
+        "price": 20,
+        "image": "a5100.jpg"
+    },
+    {
+        "name": "a5100 Beginner E-mount camera w/Tripod",
+        "price": 30,
+        "image": "a5100.jpg"
+    }
+]
+var a9FullframeCamera = [
+    {
+        "name": "a9 Full-frame camera Camera Only",
+        "price": 100,
+        "image": "a92.jpg"
+    },
+    {
+        "name": "a9 Full-frame camera w/SD card",
+        "price": 110,
+        "image": "a92.jpg"
+    },
+    {
+        "name": "a9 Full-frame camera w/Tripod",
+        "price": 120,
+        "image": "a92.jpg"
+    }
+]
+var a1Flagshipcamera = [
+    {
+        "name": "a1 Flagship camera Camera Only",
+        "price": 130,
+        "image": "Fx3.jpg"
+    },
+    {
+        "name": "a1 Flagship camera w/SD card",
+        "price": 140,
+        "image": "Fx3.jpg"
+    },
+    {
+        "name": "a1 Flagship camera w/Tripod",
+        "price": 150,
+        "image": "Fx3.jpg"
+    }
+]
